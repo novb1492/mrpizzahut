a0bf8ea826c4c34a5ccc3ca1153595eb2eb5b84c
novb1492 <novb1492@naver.com>
2021-11-15 18:24:11 +0900

    db에 이제 넣어보자

--- src/main/java/com/mrpizzahut/app/admin/menu/adminService.java
+++ src/main/java/com/mrpizzahut/app/admin/menu/adminService.java
@@ -1,5 +1,9 @@
 package com.mrpizzahut.app.admin.menu;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,7 +33,32 @@ public class adminService {
 		String text=request.getParameter("text");
 		String price=request.getParameter("price");
 		String[] sizes=confrimSize(size);
+		String[] edges=confrimEdge(edge);
+		confrimPrice(price);
+		Map<String, Object>product=new HashMap<String, Object>();
+		for(String s:sizes) {
+			product.put("productName", title);
+			product.put("stitle", stitle);
+			product.put("ititle", ititle);
+			product.put("text", text);
+			product.put("price", price);
+			product.put("size", s);
+			System.out.println(product.toString());
+		}
 		return null;
+	}
+	private void confrimPrice(String price) {
+		System.out.println("confrimPrice");
+		if(utillService.checkNull(price)) {
+			throw utillService.makeRuntimeEX("금액이 빈칸입니다", "confrimPrice");
+		}else if(price.length()<=3) {
+			System.out.println("천원 이하 제품");
+			return;
+		} 
+		if(!price.contains(",")) {
+			throw utillService.makeRuntimeEX("금액 구분문자는 ,입니다 현재"+price, "confrimPrice");
+		}
+		System.out.println("금액 유효성 검사 통과");
 	}
 	private String[] confrimSize(String size) {
 		System.out.println("confrimSize");
@@ -38,7 +67,7 @@ public class adminService {
 		}
 		String[] sizes=size.split(",");
 		for(String s:sizes) {
-			if(s.length()<1||s.length()<2) {
+			if(s.length()<1||s.length()>2) {
 				throw utillService.makeRuntimeEX("사이즈는 1~2글자입니다 잘못된 사이즈"+s, "confrimSize");
 			}
 			char a=s.charAt(0);
@@ -49,5 +78,13 @@ public class adminService {
 		}
 		System.out.println("사이즈 유효성 검사 통과");
 		return sizes;
+	}
+	private  String[] confrimEdge(String edge) {
+		System.out.println("confrimEdge");
+		if(utillService.checkNull(edge)) {
+			throw utillService.makeRuntimeEX("엣지를 입력해주세요", "confrimSize");
+		}
+		System.out.println("엣지유효성 통과");
+		return edge.split(",");		
 	}
 }
